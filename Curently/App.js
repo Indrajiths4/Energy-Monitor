799c44aac0fa325bfaa5ab520ac5ac1e39e4f9b8
Indrajiths4 <indrajithmundackal@gmail.com>
2024-03-29 19:28:19 +0530

    Authetication Done

--- Curently/App.js
+++ Curently/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { SafeAreaView } from 'react-native';
 import HomeScreen from './Screens/Home';
+import Login from './Screens/Login';
+
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <HomeScreen />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" options={{ headerShown: false }} component={Login} />
+        <Stack.Screen name="Home" options={{ headerShown: false }} component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
