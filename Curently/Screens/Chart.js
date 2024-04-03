d9975c3e28b7f9a58c921f4a26f537605aae5125
Kiran Babu <kiranbabu3355@gmail.com>
2024-04-03 18:34:05 +0530

    Add dependencies and refactor HomeScreen component

---
+++ Curently/Screens/Chart.js
@@ -0,0 +1,84 @@
+import { ref, onValue } from 'firebase/database';
+import database from '../firebaseConfig';
+import React, { useEffect, useState } from 'react';
+import { View, Dimensions } from 'react-native';
+import { LineChart } from 'react-native-chart-kit';
+
+const Chart = () => {
+
+    const [powerValue, setPowerValue] = useState('');
+    const [newdata, setNewData] = useState([2, 3, 2, 3]);
+
+    useEffect(() => {
+        const userDataRef = ref(database, '/UsersData/OUP0gXmVjCcD4p3NJNaudwQs7Er1');
+
+        const unsubscribe = onValue(userDataRef, (snapshot) => {
+            const data = snapshot.val();
+            if (data) {
+
+                let newDataValue = data.current <= 0.28 ? 0 : data.current;
+                setNewData(prevData => {
+                    const newData = [...prevData, newDataValue];
+                    const maxDataPoints = 6; 
+                    if (newData.length > maxDataPoints) {
+                       
+                        return newData.slice(newData.length - maxDataPoints);
+                    }
+                    return newData;
+                });
+            }
+        });
+
+        return () => {
+            unsubscribe();
+        };
+    }, []);
+
+
+    return (
+        <View style={{
+            marginHorizontal: 20,
+            marginTop: 40
+        }}>
+            <LineChart
+                data={{
+                    labels: ["Jan", "Feb", "Mar", "Apr"],
+                    datasets: [
+                        {
+                            data:
+                                newdata
+
+                        },
+                    ],
+                }}
+                width={Dimensions.get('window').width}
+                height={220}
+                yAxisSuffix='k'
+                yAxisInterval={1}
+                chartConfig={{
+                    backgroundColor: "#FFF",
+                    backgroundGradientFrom: "#FFF",
+                    backgroundGradientTo: "#FFF",
+                    decimalPlaces: 2,
+                    color: (opacity = 0) => `rgba(255,0,0, ${opacity})`,
+                    labelColor: (opacity = 0) => `rgba(0,0,0, ${opacity})`,
+                    style: {
+                        borderRadius: 16
+                    },
+                    propsForDots: {
+                        r: '6',
+                        strokeWidth: "2",
+                        stroke: "red"
+                    },
+                }}
+                bezier
+                style={{
+                    marginVertical: 8,
+                    borderRadius: 16
+                }}
+            />
+        </View>
+    );
+};
+
+export default Chart;
--- Curently/Screens/Home.js
+++ Curently/Screens/Home.js
@@ -4,11 +4,12 @@ import { ref, onValue } from 'firebase/database';
 import database from '../firebaseConfig';
 import Navbar from './Navbar';
 import { LineChart } from 'react-native-svg-charts';
+import Chart from './Chart';
 
 const HomeScreen = () => {
     const [currentValue, setCurrentValue] = useState('');
-    const [powerValue, setPowerValue] = useState('');
-    const [newdata, setNewData] = useState([]);
+    
+
     useEffect(() => {
         const userDataRef = ref(database, '/UsersData/OUP0gXmVjCcD4p3NJNaudwQs7Er1');
 
@@ -16,10 +17,7 @@ const HomeScreen = () => {
             const data = snapshot.val();
             if (data) {
                 setCurrentValue(data.current);
-                setPowerValue(data.power);
-                let newDataValue = data.current <= 0.28 ? 0 : data.current;
-                setNewData(prevData => [...prevData, newDataValue]);
-                console.log(newDataValue)
+                
             }
         });
 
@@ -27,11 +25,11 @@ const HomeScreen = () => {
             unsubscribe();
         };
     }, []);
-    
+
+
     return (
         <View style={styles.container}>
             <Navbar />
-
             <View style={styles.imageContainer}>
                 <Image
                     source={require('../assets/home.png')} // Provide the path to your image
@@ -42,13 +40,8 @@ const HomeScreen = () => {
 
             <Text style={styles.text}>Current Consumption:</Text>
             <Text style={styles.currenttext}>{currentValue} A</Text>
-            <View style={{}}>
-                <LineChart
-                    style={{ width: 300, height: 200 }}
-                    data={newdata}
-                    svg={{ stroke: 'rgb(134, 65, 244)' }}
-                    contentInset={{ top: 20, bottom: 20 }}
-                />
+            <View>
+                <Chart />
             </View>
         </View>
     );
@@ -73,7 +66,6 @@ const styles = StyleSheet.create({
     imageContainer: {
         height: 200,
     },
-
     image: {
         flex: 1,
         width: '100%',
