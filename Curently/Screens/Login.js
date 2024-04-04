732f3e4422f70766b8b4c8a8ec39ee76e180f41b
Sidharth-Manikuttan <sidharthmanikuttan9@gmail.com>
2024-04-04 15:03:22 +0530

    logincolor

--- Curently/Screens/Login.js
+++ Curently/Screens/Login.js
@@ -78,7 +78,7 @@ const Login = () => {
         >
           <Text style={[styles.buttonText, { color: 'white' }]} >Sign up</Text>
         </Pressable> : <Pressable
-          style={[styles.button, { backgroundColor: '#FF5767' }]}
+          style={[styles.button, { backgroundColor: 'black' }]}
           onPress={handleLogin}
         >
           <Text style={[styles.buttonText, { color: 'white' }]} >LOGIN</Text>
